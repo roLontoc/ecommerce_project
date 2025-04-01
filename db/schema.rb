97c5324f933cd6057b936164c6c786fb44c48250
Rogelyn <rlontoc39@academic.rrc.ca>
2025-04-01 09:59:43 -0500

    generated the merchandise_category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_145652) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_145904) do
   create_table "authors", force: :cascade do |t|
     t.string "author_name"
     t.datetime "created_at", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_145652) do
     t.string "last_name"
     t.string "email"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchandise_categories", force: :cascade do |t|
+    t.string "category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
