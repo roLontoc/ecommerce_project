3de0d5a8ee149112770169e1e4152675b225455c
Rogelyn <rlontoc39@academic.rrc.ca>
2025-04-08 12:10:28 -0500

    Removed filter for book images:

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,16 +1,5 @@
 ActiveAdmin.register Book do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :title, :description, :price, :stock_quantity, :author_id, :book_genre_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :description, :price, :stock_quantity, :author_id, :book_genre_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  remove_filter :image_attachment, :image_blob
 end
