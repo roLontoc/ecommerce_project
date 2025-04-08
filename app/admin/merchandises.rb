6b8c7d247e87107c2676e78853a29e1beec908d0
Rogelyn <rlontoc39@academic.rrc.ca>
2025-04-08 13:23:49 -0500

    Removed image filter and added file input

--- app/admin/merchandises.rb
+++ app/admin/merchandises.rb
@@ -1,16 +1,14 @@
 ActiveAdmin.register Merchandise do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :merch_name, :description, :price, :stock_quantity, :merchandise_category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:merch_name, :description, :price, :stock_quantity, :merchandise_category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :merch_name, :description, :price, :stock_quantity, :merchandise_category_id, :image
+
+  remove_filter :image_attachment, :image_blob
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
