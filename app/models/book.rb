d6d736b66c6b4bce04422c3ed255b61020e7c759
Rogelyn <rlontoc39@academic.rrc.ca>
2025-04-08 12:07:15 -0500

    Installed active_storage

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :book_genre
+  has_one_attached :image
 
   has_many :book_genre_assignments
   has_many :book_genres, through: :book_genre_assignments
